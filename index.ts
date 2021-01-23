eebb22529233c3381636d929384b65964b0d4627
Anwesh43 <anweshthecool0@gmail.com>
2021-01-24 03:08:58 +0530

    adding Renderer class

--- index.ts
+++ index.ts
@@ -205,3 +205,25 @@ class BoxJumpWithinStep {
         this.curr.startUpdating(cb)
     }
 }
+
+class Renderer {
+
+    curr : BoxJumpWithinStep = new BoxJumpWithinStep()
+    animator : Animator = new Animator()
+
+    render(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    handleTap(cb : Function) {
+        this.curr.startUpdating(() => {
+            this.animator.start(() => {
+                cb()
+                this.curr.update(() => {
+                    this.animator.stop()
+                    cb()
+                })
+            })
+        })
+    }
+}
